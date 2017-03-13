ef4e8d943f94ecd16fb236ea8b1deb52b337d755
Jouin Alexis <alexis.jouin@live.fr>
2017-03-13 14:38:01 +0100

    ADD : Member Et références ADD : bootstrap

---
+++ tp2_mediatheque/src/BookBundle/Entity/Member.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace BookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Member
+ *
+ * @ORM\Table(name="member")
+ * @ORM\Entity(repositoryClass="BookBundle\Repository\MemberRepository")
+ */
+class Member {
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="BookBundle\Entity\Book", cascade={"persist"})
+     * @ORM\JoinColumn(name="id_book", referencedColumnName="id")
+     */
+    private $idBook;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     * 
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Member
+     */
+    public function setName($name) {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+
+    /**
+     * Set idBook
+     *
+     * @param \BookBundle\Entity\Book $idBook
+     *
+     * @return Member
+     */
+    public function setIdBook(\BookBundle\Entity\Book $idBook = null)
+    {
+        $this->idBook = $idBook;
+
+        return $this;
+    }
+
+    /**
+     * Get idBook
+     *
+     * @return \BookBundle\Entity\Book
+     */
+    public function getIdBook()
+    {
+        return $this->idBook;
+    }
+}
