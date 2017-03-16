038d0b20038da0dbd9412c6c88e1d4a3a88e1655
Unknown <schouteeten.sebastien@gmail.com>
2017-03-16 11:17:14 +0100

    ADD : Json Edit / DELETE

--- tp2_mediatheque/src/BookBundle/Controller/BookController.php
+++ tp2_mediatheque/src/BookBundle/Controller/BookController.php
@@ -13,104 +13,78 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class BookController extends Controller
 {
-    /**
-     * Lists all book entities.
-     *
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+  /**
+   * Lists all book entities.
+   *
+   */
+  public function indexAction()
+  {
+    $em = $this->getDoctrine()->getManager();
 
-        $books = $em->getRepository('BookBundle:Book')->findAll();
+    $books = $em->getRepository('BookBundle:Book')->findAll();
 
-        return new JsonResponse(array('books' => $books));
-    }
+    return new JsonResponse(array('books' => $books));
+  }
 
-    /**
-     * Creates a new book entity.
-     *
-     */
-    public function newAction(Request $request)
-    {
-        $book = new Book();
+  /**
+   * Creates a new book entity.
+   *
+   */
+  public function newAction(Request $request)
+  {
+    $book = new Book();
 
-        $data = json_decode($request->getContent(), true);
-        $name = $data['name'];
-        $category = $data['category'];
+    $data = json_decode($request->getContent(), true);
+    $name = $data['name'];
+    $category = $data['category'];
 
-        $book->setName($name);
-        $book->setCategory($category);
+    $book->setName($name);
+    $book->setCategory($category);
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($book);
-        $em->flush($book);
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($book);
+    $em->flush($book);
 
-        return new JsonResponse($book);
-    }
+    return new JsonResponse($book);
+  }
 
-    /**
-     * Finds and displays a book entity.
-     *
-     */
-    public function showAction(Book $book)
-    {
-      return new JsonResponse($book);
-    }
+  /**
+   * Finds and displays a book entity.
+   *
+   */
+  public function showAction(Book $book)
+  {
+    return new JsonResponse($book);
+  }
 
-    /**
-     * Displays a form to edit an existing book entity.
-     *
-     */
-    public function editAction(Request $request, Book $book)
-    {
-        $deleteForm = $this->createDeleteForm($book);
-        $editForm = $this->createForm('BookBundle\Form\BookType', $book);
-        $editForm->handleRequest($request);
+  /**
+   * Displays a form to edit an existing book entity.
+   *
+   */
+  public function editAction(Request $request, Book $book)
+  {
+    $data = json_decode($request->getContent(), true);
+    $name = $data['name'];
+    $category = $data['category'];
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+    $book->setName($name);
+    $book->setCategory($category);
 
-            return $this->redirectToRoute('books_edit', array('id' => $book->getId()));
-        }
+    $this->getDoctrine()->getManager()->flush();
 
-        return $this->render('book/edit.html.twig', array(
-            'book' => $book,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+    return new JsonResponse($book);
+  }
 
-    /**
-     * Deletes a book entity.
-     *
-     */
-    public function deleteAction(Request $request, Book $book)
-    {
-        $form = $this->createDeleteForm($book);
-        $form->handleRequest($request);
+  /**
+   * Deletes a book entity.
+   *
+   */
+  public function deleteAction(Request $request, Book $book)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $em->remove($book);
+    $em->flush();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($book);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('books_index');
-    }
-
-    /**
-     * Creates a form to delete a book entity.
-     *
-     * @param Book $book The book entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Book $book)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('books_delete', array('id' => $book->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
+    return new JsonResponse("Livre bien supprimé !");
+  }
 }
